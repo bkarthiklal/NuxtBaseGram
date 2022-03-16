b4d4649daaa3993f8085579590bcf586905fb3c2
Karthik <bkarthiklal@gmail.com>
2022-03-16 22:39:46 +0530

    Installed and registered VueSlickCarousel

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,10 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ['~/plugins/VueDragscroll'],
+  plugins: [
+    '~/plugins/VueDragscroll',
+    { src: '~/plugins/VueSlickCarousel', mode: 'client' },
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
